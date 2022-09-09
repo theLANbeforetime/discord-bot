96e3d12ed55179a7d3c0402f56f535923670f0f4
theLANbeforetime <traxxon.stecklein@gmail.com>
2022-09-08 18:00:40 -0600

    Adding some documentation

--- bot/bot.go
+++ bot/bot.go
@@ -33,6 +33,8 @@ func Run() {
 		return
 	}
 }
+
+// Reads in desired user search term and then utilizes helix twitch library search top 20 users and return if they are live and playing Apex Legends
 func MessageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore all messages created by the bot itself
 	if m.Author.ID == BotID {
@@ -47,6 +49,7 @@ func MessageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if err != nil {
 			panic(err)
 		}
+		// Search Twitch.tv for the first 20 channels similar to search name
 		resp, err := client.SearchChannels(&helix.SearchChannelsParams{
 			First:   20,
 			Channel: searchTerm,
@@ -54,7 +57,7 @@ func MessageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if err != nil {
 			//handle error
 		}
-
+		// Write message to Discord to show search results
 		_, _ = s.ChannelMessageSend(m.ChannelID, "Below are the users who matched for Apex Legends:\n")
 		for _, respons := range resp.Data.Channels {
 			if respons.GameName == "Apex Legends" {
